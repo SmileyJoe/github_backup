a492bff33bb9c17f6d87cf0cd2e5c4428bcc3465
Cody Strugnell <cody.strugnell@gmail.com>
2021-07-11 14:40:11 +0200

    Moving pushover to log

--- log.py
+++ log.py
@@ -1,16 +1,42 @@
+from pushover import Client
+
+
 class Log:
 
     def __init__(self, config):
-        self._enabled = config.logging.enabled
+        self._config = config.logging
+        self._pushover = config.pushover
+        self._count_cloned = 0
+        self._count_updated = 0
+        self._count_skipped = 0
 
     def updated(self, name):
         self._repo(name, self._Colors.YELLOW, "updated")
+        self._count_updated += 1
 
     def skipped(self, name):
         self._repo(name, self._Colors.RED, "skipped")
+        self._count_skipped += 1
 
     def cloned(self, name):
         self._repo(name, self._Colors.GREEN, "cloned")
+        self._count_cloned += 1
+
+    def pushover(self):
+        pushover = Client(self._pushover.user, api_token=self._pushover.api_token)
+        if self._count_cloned > 0 or self._count_updated > 0 or self._count_skipped > 0:
+            message = "Cloned: {cloned} \nUpdated: {updated} \nSkipped: {skipped}"\
+                .format(cloned=self._count_cloned,
+                        updated=self._count_updated,
+                        skipped=self._count_skipped)
+            title = "Success!"
+        else:
+            message = "Something went wrong"
+            title = "Failed!"
+
+        pushover.send_message(message,
+                              title="Github sync {title}".format(title=title),
+                              priority=-1)
 
     def _repo(self, name, color, action):
         self.log("{name} : {color}{action}{end_color}"
@@ -20,7 +46,7 @@ class Log:
                          end_color=self._Colors.END))
 
     def log(self, message):
-        if self._enabled:
+        if self._config.enabled:
             print(message)
 
     class _Colors:
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from github import Github
 from git import Repo
-from pushover import Client
 from config import Config
 from log import Log
 from db import Db
@@ -12,13 +11,9 @@ config = Config()
 db = Db()
 log = Log(config)
 github = Github(config.github.token)
-pushover = Client(config.pushover.user, api_token=config.pushover.api_token)
 
 last_run = datetime.fromisoformat(db.last_run)
 time_start = datetime.now().astimezone(timezone.utc)
-count_cloned = 0
-count_updated = 0
-count_skipped = 0
 
 for repo in github.get_user().get_repos(affiliation="owner", sort="updated", direction="direction"):
     updated_at = repo.updated_at.replace(tzinfo=timezone.utc)
@@ -29,23 +24,13 @@ for repo in github.get_user().get_repos(affiliation="owner", sort="updated", dir
         if not os.path.isdir(repo_dir):
             Repo.clone_from(repo.ssh_url, repo_dir)
             log.cloned(repo.name)
-            count_cloned += 1
         else:
             Repo(repo_dir).remotes.origin.pull()
             log.updated(repo.name)
-            count_updated += 1
     else:
         log.skipped(repo.name)
-        count_skipped += 1
 
-if count_cloned > 0 or count_updated > 0 or count_skipped > 0:
-    message = "Cloned: {cloned} \nUpdated: {updated} \nSkipped: {skipped}".format(cloned=count_cloned, updated=count_updated, skipped=count_skipped)
-    title = "Success!"
-else:
-    message = "Something went wrong"
-    title = "Failed!"
-
-pushover.send_message(message, title="Github sync {title}".format(title=title), priority=-1)
+log.pushover()
 
 db.last_run = time_start
 db.save()
